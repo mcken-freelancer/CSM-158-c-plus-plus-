73e9537850d622e4ea46bc64feb89bdc5b4aa3b7
mcken-freelancer <mcken-freelancer@users.noreply.github.com>
2025-02-01 17:14:22 +0000

    chale

---
+++ broCodeIfs.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class broCodeIfs {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        String name;
+       
+       System.out.println("Enter your name: ");
+       name=scanner.nextLine();
+
+      
+        for (int i = 0; i > -1 ;i++ ) {
+            if (name.isEmpty()) {
+                System.out.println("This field is compulsury");
+                System.out.println("Enter your name: ");
+                name=scanner.nextLine();
+            }
+            else{
+                System.out.println("Hello "+name);
+               break;
+            }
+        }
+      
+        scanner.close();
+    }
+}
+ 
